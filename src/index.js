906425d3da2a71f5b976f111fd9bea38cb841385
asadciit015 <asadciit015@gmail.com>
2024-05-28 04:32:14 +0500

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+"use client";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppProvider } from "./store/AppContext";
-
+import { HashRouter } from "react-router-dom";
 import { IntlProvider } from "react-intl";
 
 ReactDOM.render(
   <React.StrictMode>
     <IntlProvider locale="en">
       <AppProvider>
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </AppProvider>
     </IntlProvider>
   </React.StrictMode>,
